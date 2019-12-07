c0ac5759c73277de45e8b30dff23ddf2b233f7bc
Aishwarya <aishwaryadiga@gmail.com>
2019-12-07 16:43:22 +0530

    Add feed api to retrofit interface

--- app/src/main/java/com/app/androidexercise/data/network/ApiInterface.java
+++ app/src/main/java/com/app/androidexercise/data/network/ApiInterface.java
@@ -1,4 +1,15 @@
 package com.app.androidexercise.data.network;
 
+import androidx.lifecycle.LiveData;
+
+import com.app.androidexercise.data.network.model.FeedResponse;
+import com.github.leonardoxh.livedatacalladapter.Resource;
+
+import retrofit2.http.GET;
+
 public interface ApiInterface {
+
+    @GET("/s/2iodh4vg0eortkl/facts.json")
+    LiveData<Resource<FeedResponse>> getFeeds();
+
 }
