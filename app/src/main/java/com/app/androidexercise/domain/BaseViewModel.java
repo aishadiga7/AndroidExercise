ce75517586ac12cffada1a30d55a23ee9f32047a
Aishwarya <aishwaryadiga@gmail.com>
2019-12-07 21:26:17 +0530

    Add loader observable in base view model

--- app/src/main/java/com/app/androidexercise/domain/BaseViewModel.java
+++ app/src/main/java/com/app/androidexercise/domain/BaseViewModel.java
@@ -1,6 +1,19 @@
 package com.app.androidexercise.domain;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public abstract class BaseViewModel extends ViewModel {
+
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
+
+
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
+    }
+
+    public void setLoading(boolean isLoading) {
+        this.isLoading.postValue(isLoading);
+    }
 }
--- app/src/main/java/com/app/androidexercise/domain/home/HomeScreenViewModel.java
+++ app/src/main/java/com/app/androidexercise/domain/home/HomeScreenViewModel.java
@@ -17,4 +17,6 @@ public class HomeScreenViewModel extends BaseViewModel {
     }
 
 
+
+
 }
