cf39854850474452424e4997edf25ae38e767291
Aishwarya <aishwaryadiga@gmail.com>
2019-12-07 19:34:12 +0530

    Add databinding for MainActivity

--- app/src/main/java/com/app/androidexercise/ui/MainActivity.java
+++ app/src/main/java/com/app/androidexercise/ui/MainActivity.java
@@ -1,16 +1,25 @@
 package com.app.androidexercise.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.app.androidexercise.R;
+import com.app.androidexercise.databinding.ActivityMainBinding;
+import com.app.androidexercise.domain.home.HomeScreenViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mMainBinding;
+    private HomeScreenViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mViewModel = new HomeScreenViewModel();
+        mMainBinding.setViewModel(mViewModel);
+        mMainBinding.setLifecycleOwner(this);
     }
 }
