6874bac0095f6e20b39ca08a7b9d31c2836a6802
Aishwarya <aishwaryadiga@gmail.com>
2019-12-08 17:25:47 +0530

    Add BindingAdapter for swipe refresh

--- app/src/main/java/com/app/androidexercise/ui/DataBindingUtilily.java
+++ app/src/main/java/com/app/androidexercise/ui/DataBindingUtilily.java
@@ -3,6 +3,7 @@ package com.app.androidexercise.ui;
 import android.widget.ImageView;
 
 import androidx.databinding.BindingAdapter;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.app.androidexercise.R;
 import com.bumptech.glide.Glide;
@@ -18,5 +19,10 @@ public class DataBindingUtilily {
                 .placeholder(R.drawable.no_image)
                 .error(requestManager.load(R.drawable.no_image))
                 .into(view);
+    }
+
+    @BindingAdapter("refreshing")
+    public static void refreshing(SwipeRefreshLayout swipeRefreshLayout, boolean isRefreshing) {
+        swipeRefreshLayout.setRefreshing(isRefreshing);
     }
 }
